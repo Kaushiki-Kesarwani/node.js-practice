1dda22c8ac397e5e23589869adf492d1a434520a
mahak <mahakkesarwani0@gmail.com>
2026-02-09 17:10:18 +0530

    add files

---
+++ app.js
@@ -0,0 +1,14 @@
+// console.log(2+2);
+// let greet = (name)=>{
+//     console.log(`hello ${name}`);
+// }
+
+// greet("Palak");
+
+// let a = 10;
+// let b = 20;
+
+// console.log(`sum : ${a+b}`);
+
+let fs = require('fs');
+fs.writeFileSync("palak.txt","i am palak");
