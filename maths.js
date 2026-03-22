87234244c6740f70728873ffcb81fab11a5960a0
mahak <kaushikikes@gmail.com>
2026-03-22 13:50:29 +0530

    custom modules

---
+++ maths.js
@@ -0,0 +1,36 @@
+//add
+// const add = (a,b)=>{
+//     return a+b;
+// }
+
+//subtract
+// const subtract = (a,b)=>{
+//     return a-b;
+// }
+
+//Multiplication
+// const Multiplication = (a,b)=>{
+//     return a*b;
+// }
+
+//Division
+// const Division = (a,b)=>{
+//     return a/b;
+// }
+
+
+// module.exports = {
+//     add,
+//     subtract,
+//     Multiplication,
+//     Division
+// };
+
+
+exports.add = (a,b) => a+b;
+
+exports.subtract = (a,b) =>a-b;
+
+exports.Multiplication = (a,b) => a*b;
+
+exports.Division = (a,b) => a/b;
---
+++ usingCustomModules.js
@@ -0,0 +1,11 @@
+const {add,subtract,Multiplication,Division} = require('./maths');
+
+// console.log(maths.add(6,2));
+// console.log(maths.subtract(6,2));
+// console.log(maths.Multiplication(6,2));
+// console.log(maths.Division(6,2));
+
+console.log(add(9,1));
+console.log(subtract(5,1));
+console.log(Multiplication(2,1));
+console.log(Division(4,2));
