8272763b44c92b417c676a77436187df4169887c
mahak <kaushikikes@gmail.com>
2026-03-26 22:48:37 +0530

    add comments

--- file.js
+++ file.js
@@ -1,3 +1,14 @@
 const fs = require('fs');
 
-fs.writeFileSync("./test.txt","hello my world");
+// fs.writeFileSync("./test.txt","hello my world"); 
+fs.readFile("./test.txt","utf-8",(err,res)=>{//always use this
+    if(err){
+        console.error(err);
+    }
+    else{
+        console.log(res);
+    }
+});
+
+// const res = fs.readFileSync("./test.txt","utf-8");
+// console.log(res);
