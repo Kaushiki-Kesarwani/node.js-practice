bfac85c0b5cd5e4c93046b04b446b00772dd7039
mahak <kaushikikes@gmail.com>
2026-03-27 15:47:58 +0530

    add comment

--- file.js
+++ file.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 // fs.writeFileSync("./test.txt","hello my world"); 
-fs.readFile("./test.txt","utf-8",(err,res)=>{
+fs.readFile("./test.txt","utf-8",(err,res)=>{ 
     if(err){
         console.error(err);
     }
